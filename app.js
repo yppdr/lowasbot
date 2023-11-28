d83b7a84c053a35a464dceb0a6efddb54cb80e46
Yannis Piot Pilot <yannis@piotpilot.eu>
2023-11-28 15:35:44 +0100

    WIP

--- app.js
+++ app.js
@@ -18,13 +18,13 @@ client.on('message', (channel, tags, message, self) => {
 	// Ignore echoed messages.
 	if(self) return;
 
+	// CMD pour dire hello
 	if(message.toLowerCase() === 'hello') {
-		// "@alca, heya!"
 		client.say(channel, `Hello @${tags.username} !`);
 	}
 
+	// CMD pour 21
 	if(message.toLowerCase() === '!21') {
-		// "@alca, heya!"
 		client.say(channel, `@${tags.username} 21JumpClick est un serveur GTARP public avec whitelist ! plus d'info ici : https://kiwinetwork.xyz/21`);
 	}
 
